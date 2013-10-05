5e0850515c8e7eda2b3c4da0d303c969af935e92
Kevin Fanning <kmf5302@gmail.com>
2013-10-04 20:59:16 -0400

    initial commit

---
+++ FakeDLL/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsPhoneUnityPlugin
+{
+    public class Class1
+    {
+        public static String getDeviceName
+        {
+            get
+            {
+                return "Not a windows phone.";
+            }
+        }
+    }
+}
