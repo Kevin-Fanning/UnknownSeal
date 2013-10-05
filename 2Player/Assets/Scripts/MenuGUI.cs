e54c8c560a99093b8fd97ebb928e6cbd10980d11
Kevin Fanning <kmf5302@gmail.com>
2013-10-04 21:23:30 -0400

    Project skeleton

---
+++ 2Player/Assets/Scripts/MenuGUI.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuGUI : MonoBehaviour {
+
+	void OnGUI()
+	{
+		GUI.Label (new Rect(10, 10, 200, 30), "Unknown Seal");
+		
+		if (GUI.Button (new Rect(10, 50, 200, 200), "Start Game"))
+		{
+			Application.LoadLevel (1);	
+		}
+		
+		
+	}
+}
